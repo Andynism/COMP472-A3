16676e1a4839b3cf96cbe1f8f8041915b05e1020
MikeBrichko <w.mike.brichko@gmail.com>
2020-11-30 15:46:34 -0500

    Add filetered vocabulary

--- vocabulary.py
+++ vocabulary.py
@@ -1,17 +1,30 @@
 import csv
 
-fd = open('./datasets/covid_training.tsv', encoding="mbcs")
-rd = csv.reader(fd, delimiter="\t")
+class Vocabulary:
+    def __init__(self, dataset, filtered):
+        self.vocabulary_dictionnary = {}
+        self.vocabulary_size = 0
+        tsv_file = csv.reader(open(dataset, encoding="mbcs"), delimiter="\t")
+        for row in tsv_file:
+            for word in row[1].split():
+                if(word in self.vocabulary_dictionnary):
+                    self.vocabulary_dictionnary[word] = self.vocabulary_dictionnary.get(word) + 1
+                else: 
+                    self.vocabulary_dictionnary[word] = 1
+                    self.vocabulary_size += 1
 
-vocabulary = {}
-vocabulary_size = 0
-for row in rd:
-    for word in row[1].split():
-        if(word in vocabulary):
-            vocabulary[word] = vocabulary.get(word) + 1
-        else: 
-            vocabulary[word] = 1
-            vocabulary_size += 1
+        if(filtered):
+            keys_to_delete = []
+            for (key,value) in self.vocabulary_dictionnary.items():
+                if(value == 1):
+                    keys_to_delete.append(key)
+                    self.vocabulary_size -= 1
+            
+            for key in keys_to_delete:
+                del self.vocabulary_dictionnary[key]
 
-print(vocabulary)
-print(vocabulary_size)
+v = Vocabulary('./datasets/covid_training.tsv', True)
+print(v.vocabulary_dictionnary)
+print(v.vocabulary_size)
+
+
